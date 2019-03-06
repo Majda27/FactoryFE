338264599b122a48444b9028c2a6495e3b179223
neorey <jeroenvdsteen@hotmail.fr>
2019-03-06 15:46:20 +0100

    avec les routings

--- FactoryFE/factory-angular/src/app/app-routing.module.ts
+++ FactoryFE/factory-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormateurComponent } from './formateur/formateur.component';
+import { MatiereComponent } from './matiere/matiere.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'formateur', component: FormateurComponent },
+  { path: 'matiere', component: MatiereComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FactoryFE/factory-angular/src/app/sidebar/sidebar.component.ts
+++ FactoryFE/factory-angular/src/app/sidebar/sidebar.component.ts
@@ -8,16 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   config = {
     panels: [
-      { name: 'Ordinateur', description: 'Actions' },
-      { name: 'Video projecteur', description: 'Actions' },
-      { name: 'Salles', description: 'Actions' },
-      { name: 'Gestionnaire', description: 'Actions' },
-      { name: 'Technicien', description: 'Actions' },
-      { name: 'Formateur', description: 'Actions' },
-      { name: 'Stagiaire', description: 'Actions' },
-      { name: 'Matières', description: 'Actions' },
-      { name: 'Modules', description: 'Actions' },
-      { name: 'Formations', description: 'Actions' },
+      { name: 'Ordinateur', description: 'Actions', path: '/ordinateur' },
+      { name: 'Video projecteur', description: 'Actions', path: '/videoproj' },
+      { name: 'Salles', description: 'Actions', path: '/salle' },
+      { name: 'Gestionnaire', description: 'Actions', path: '/formateur' },
+      { name: 'Technicien', description: 'Actions', path: '/formateur' },
+      { name: 'Formateur', description: 'Actions', path: '/matiere' },
+      { name: 'Stagiaire', description: 'Actions', path: '/formateur' },
+      { name: 'Matières', description: 'Actions', path: '/formateur' },
+      { name: 'Modules', description: 'Actions', path: '/formateur' },
+      { name: 'Formations', description: 'Actions', path: '/formateur' },
     ]
   };
   constructor() { }
