7fb522aa7a1ef52952503a1566deb5c9939b24c1
neorey <jeroenvdsteen@hotmail.fr>
2019-03-06 11:20:28 +0100

    changement de navigation

--- FactoryFE/factory-angular/src/app/sidebar/sidebar.component.ts
+++ FactoryFE/factory-angular/src/app/sidebar/sidebar.component.ts
@@ -8,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class SidebarComponent implements OnInit {
   config = {
     panels: [
+      { name: 'Ordinateur', description: 'Actions' },
+      { name: 'Video projecteur', description: 'Actions' },
+      { name: 'Salles', description: 'Actions' },
       { name: 'Gestionnaire', description: 'Actions' },
-      { name: 'Formateur', description: 'Actions' },
       { name: 'Technicien', description: 'Actions' },
+      { name: 'Formateur', description: 'Actions' },
       { name: 'Stagiaire', description: 'Actions' },
+      { name: 'Matières', description: 'Actions' },
+      { name: 'Modules', description: 'Actions' },
+      { name: 'Formations', description: 'Actions' },
     ]
   };
   constructor() { }
